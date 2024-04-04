22da862b8aab2ba7a41d6123fa801aa65222f148
jjcastle1992 <77635923+jjcastle1992@users.noreply.github.com>
2024-04-04 16:57:43 -0500

    Add placeholders for all Factory methods. Constructor defined,initializeBuffer also defined.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,71 @@ Purpose: Implementaation of the Producer-Consumer Problem (Project 4)
 **********************/
 #include "main.h"
 
+Factory::Factory() {
+    const int bufferSize = 5;
+    int itemsProduced = 0;
+    int itemsConsumed = 0;
+    mutex bufferLock; // to ensure mutual exclusion during buffer access by prod or consumer
+    counting_semaphore<bufferSize> full(0); // count number of full spaces
+    counting_semaphore<bufferSize> empty(bufferSize);  // count number of empty spaces in buffer
+    int boundedBuffer[bufferSize];
+}
+
+void Factory::makeProducer() {
+    ;
+}
+
+void Factory::makeConsumer() {
+    ;
+}
+
+bool Factory::bufferAvailable() {
+    ;
+}
+
+bool Factory::insert_item() {
+    // Inserts a random number in the buffer (0 - RAND_MAX)
+    ;
+}
+
+bool Factory::remove_item() {
+    ;
+}
+
+void Factory::initializeLocks() {
+    ;
+}
+
+void Factory::initializeBuffer() {
+    for(int & i : this->boundedBuffer) {
+        i = -1; // Initialize each item to "-1" or empty
+    }
+}
+
+void Factory::displayBuffer() {
+    ;
+}
+
+int randomRangeGen(int endRange, int startRange = 0, unsigned int seed = 42) {
+    // General implementation borrowed from:
+    // https://www.digitalocean.com/community/tutorials/random-number-generator-c-plus-plus
+    int random;
+
+    // Random pathway
+    if (seed == 42) {
+        random = startRange + (rand() % ((endRange - startRange) + 1));
+    }
+        // Set seed pathway
+    else {
+        // Modified with ChatGPT to take in a seed.
+        // Initialize the random number generator with the provided seed
+        std::mt19937 gen(seed);
+        // Retrieve a random number between startRange and EndRange
+        random = startRange + (gen() % ((endRange - startRange) + 1));
+    }
+    return random;
+}
+
 int main() {
 
     // Get inputs RE:
@@ -13,8 +78,10 @@ int main() {
         // 3. Number of consumer threads
 
     // Build factory
+    Factory ourFactory;
 
-    // Check buffer empty
+    // Initialize Buffer
+    ourFactory.initializeBuffer();
 
     // Check Semaphores ready
 
@@ -23,7 +90,9 @@ int main() {
     // Create Consumers
 
     // Sleep
-    
+
     // Exit
+    cout << "Done" << endl;
+
     return 0;
 }
